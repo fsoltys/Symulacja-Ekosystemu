798b8f749a602adb36d54be28dd2f3d66cb82a23
veneviusxx <75221943+veneviusxx@users.noreply.github.com>
2023-05-30 17:36:04 +0200

    Update Prey.java

--- src/main/java/org/example/Prey.java
+++ src/main/java/org/example/Prey.java
@@ -1,8 +1,26 @@
 package org.example;
+import java.util.Random;
 
-public class Prey extends Animal{
-    public Prey(int row, int col, char symbol){
+public abstract class Prey extends Animal {
+    public Prey(int row, int col, char symbol) {
         super(row, col, symbol);
     }
 
+    public void move(Board board) {
+        int currentX = getX();
+        int currentY = getY();
+
+        // Przesuwa ofiarę o jedno pole w losowym kierunku
+        Random random = new Random();
+        int dx = random.nextInt(3) - 1; // -1, 0 lub 1
+        int dy = random.nextInt(3) - 1; // -1, 0 lub 1
+
+        int newX = currentX + dx;
+        int newY = currentY + dy;
+
+        if (board.isValidPosition(newX, newY)) {
+            setX(newX);
+            setY(newY);
+        }
+    }
 }
