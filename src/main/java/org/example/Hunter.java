aa79ac845f225793a0e74eb28b5bf530548ebb03
veneviusxx <75221943+veneviusxx@users.noreply.github.com>
2023-05-30 17:32:59 +0200

    Update Hunter.java

--- src/main/java/org/example/Hunter.java
+++ src/main/java/org/example/Hunter.java
@@ -1,9 +1,27 @@
 package org.example;
+import java.util.Random;
 
 public class Hunter extends Animal {
-
-    public Hunter(int row, int col, char symbol){
-
+    public Hunter(int row, int col, char symbol) {
         super(row, col, symbol);
+    }
+
+    @Override
+    public void move(Board board) {
+        int currentX = getX();
+        int currentY = getY();
+
+        // Przesuwa drapieżnika o jedno pole w losowym kierunku
+        Random random = new Random();
+        int dx = random.nextInt(3) - 1; // -1, 0 lub 1
+        int dy = random.nextInt(3) - 1; // -1, 0 lub 1
+
+        int newX = currentX + dx;
+        int newY = currentY + dy;
+
+        if (board.isValidPosition(newX, newY)) {
+            setX(newX);
+            setY(newY);
+        }
     }
 }
