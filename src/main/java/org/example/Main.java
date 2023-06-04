f06de68bbee0b53dcafe158cf78de24f4a045793
FilipTym <133815419+FilipTym@users.noreply.github.com>
2023-06-04 20:51:02 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,73 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(10, 10);
 
-        // Tworzenie obiektów zwierząt
-        Prey antelope = new Antelope(3, 4, 'A');
-        Prey zebra = new Zebra(5, 6, 'Z');
-        Hunter jackal = new Jackal(7, 8, 'J');
-        Hunter lion = new Lion(9, 9, 'L');
-
-        // Umieszczanie zwierząt na planszy
-        board.placeAnimal(antelope);
-        board.placeAnimal(zebra);
-        board.placeAnimal(jackal);
-        board.placeAnimal(lion);
-
-        // Przed ruchem
-        System.out.println("Plansza przed ruchem:");
-        board.printBoard();
-
-        // Metoda move() dla wszystkich zwierząt
-        List<Animal> animals = board.getAnimals();
-        for (Animal animal : animals) {
-            if (animal instanceof Prey) {
-                Prey prey = (Prey) animal;
-                prey.move(board);
-            } else if (animal instanceof Hunter) {
-                Hunter hunter = (Hunter) animal;
-                hunter.move(board);
-            }
+        // Add animals to the board
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            int x = random.nextInt(10);
+            int y = random.nextInt(10);
+            Prey antelope = new Antelope(x, y, 'A');
+            board.placeAnimal(antelope);
         }
 
-        // Aktualizacja
-        board.updateBoard();
+        for (int i = 0; i < 5; i++) {
+            int x = random.nextInt(10);
+            int y = random.nextInt(10);
+            Prey zebra = new Zebra(x, y, 'Z');
+            board.placeAnimal(zebra);
+        }
 
-        // Po ruchu
-        System.out.println("Plansza po ruchu:");
+        int x = random.nextInt(10);
+        int y = random.nextInt(10);
+        Hunter hyena = new Hyena(x, y, 'H');
+        board.placeAnimal(hyena);
+
+        x = random.nextInt(10);
+        y = random.nextInt(10);
+        Hunter lion = new Lion(x, y, 'L');
+        board.placeAnimal(lion);
+
+        // Add food to the board
+        for (int i = 0; i < 3; i++) {
+            x = random.nextInt(10);
+            y = random.nextInt(10);
+            Food food = new Food(x, y);
+            board.placeFood(food);
+        }
+
+        // Print the initial board
+        System.out.println("Initial board:");
         board.printBoard();
+
+        // Update the board state for 20 iterations
+        for (int i = 0; i < 20; i++) {
+            for (Animal animal : board.getAnimals()) {
+                animal.move(board);
+            }
+            board.updateBoard();
+            System.out.println("\nIteration: " + (i + 1));
+            board.printBoard();
+        }
+
+        // Calculate and print the population of prey and hunters
+        int preyPopulation = 0;
+        int hunterPopulation = 0;
+        for (Animal animal : board.getAnimals()) {
+            if (animal instanceof Prey) {
+                preyPopulation++;
+            }
+            if (animal instanceof Hunter) {
+                hunterPopulation++;
+            }
+        }
+        System.out.println("\nFinal board:");
+        board.printBoard();
+        System.out.println("Prey population: " + preyPopulation);
+        System.out.println("Hunter population: " + hunterPopulation);
     }
-
-        public static void toFile(){
-
-    }
-
 }
