15cae9ad981d41bcb991f54dcdc847cde6bf791f
FilipTym <133815419+FilipTym@users.noreply.github.com>
2023-06-04 20:49:48 +0200

    Update Board.java

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,16 +1,18 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class Board {
     private char[][] cells;
     private List<Animal> animals;
+    private List<Food> foods;
 
     public Board(int width, int height) {
         cells = new char[width][height];
         animals = new ArrayList<>();
+        foods = new ArrayList<>();
 
         // Initialize all cells with empty value
         for (int i = 0; i < width; i++) {
@@ -31,6 +33,17 @@ public class Board {
         }
     }
 
+    // Method to place food on the board
+    public void placeFood(Food food) {
+        int x = food.getX();
+        int y = food.getY();
+
+        if (isValidPosition(x, y)) {
+            cells[x][y] = 'F';
+            foods.add(food);
+        }
+    }
+
     // Public method to check if a position is valid on the board
     public boolean isValidPosition(int x, int y) {
         int width = cells.length;
@@ -46,20 +59,52 @@ public class Board {
 
     // Method to update the board state
     public void updateBoard() {
-        Iterator<Animal> iterator = animals.iterator();
-        while (iterator.hasNext()) {
-            Animal animal = iterator.next();
+        // Clear the board
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = 'O';
+            }
+        }
+
+        // Update the board with animal positions
+        for (Animal animal : animals) {
+            int x = animal.getX();
+            int y = animal.getY();
+
+            if (isValidPosition(x, y)) {
+                cells[x][y] = animal.getSymbol();
+            }
+        }
+
+        // Update the board with food positions
+        for (Food food : foods) {
+            int x = food.getX();
+            int y = food.getY();
+
+            if (isValidPosition(x, y)) {
+                cells[x][y] = 'F';
+            }
+        }
+
+        // Process hunter-prey interactions
+        for (int i = 0; i < animals.size(); ++i) {
+            Animal animal = animals.get(i);
             int x = animal.getX();
             int y = animal.getY();
 
             if (animal instanceof Hunter) {
                 // Check if there is a prey at the same coordinates
-                for (Animal otherAnimal : animals) {
-                    if (otherAnimal instanceof Prey && otherAnimal.getX() == x && otherAnimal.getY() == y) {
+                for (int j = 0; j < animals.size(); ++j) {
+                    Animal otherAnimal = animals.get(j);
+                    if (otherAnimal != animal && otherAnimal instanceof Prey && otherAnimal.getX() == x && otherAnimal.getY() == y) {
                         // Hunter eats the prey
-                        iterator.remove();
+                        animals.remove(otherAnimal);
+                        if (i > j) {
+                            i--;
+                        }
                         cells[x][y] = animal.getSymbol();
                         System.out.println("Hunter ate the prey at (" + x + ", " + y + ")");
+                        j--;
                         break;
                     }
                 }
