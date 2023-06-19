1a54991f5badadef56900c156fcdb3afe599f8e7
Justyna Borowik <jborowik999@gmail.com>
2023-06-19 18:03:23 +0200

    Save to CSV; diagrams

---
+++ example/SimElement.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class SimElement extends Main {
+    private int x, y;
+    public SimElement(){}
+    public SimElement(int row, int column){
+        x = row;
+        y = column;
+    }
+
+    void setX(int x){
+        this.x = x;
+    }
+    void setY(int y){this.y = y;}
+
+    int getY(){
+        return y;
+    }
+    int getX(){
+        return x;
+    }
+}
